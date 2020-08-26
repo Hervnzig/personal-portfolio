1452e4ed7b4ad8739fc25718fccd8d6f44996808
Herve musangwa <hervnzig1@gmail.com>
2020-08-26 19:22:23 +0200

    a feature for displaying blog

--- scripts/dist/script.dev.js
+++ scripts/dist/script.dev.js
@@ -1,3 +1,13 @@
-// Create blog, display blog image
-
 "use strict";
+
+// Create blog, display blog image
+function preview_image(event) {
+  var reader = new FileReader();
+
+  reader.onload = function () {
+    var output = document.getElementById("output_image");
+    output.src = reader.result;
+  };
+
+  reader.readAsDataURL(event.target.files[0]);
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
 // Create blog, display blog image
+function preview_image(event) {
+  var reader = new FileReader();
+  reader.onload = function () {
+    var output = document.getElementById("output_image");
+    output.src = reader.result;
+  };
+  reader.readAsDataURL(event.target.files[0]);
+}
